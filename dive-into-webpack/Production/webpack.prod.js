e60dc8724e31bef54ecc9e3d5c86cedc7cb26157
1391020381@qq.com <1391020381@qq.com>
2019-10-28 20:45:07 +0800

    zf-webpack

--- dive-into-webpack/Production/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const UgligyJSPlugin = require('uglifyjs-webpack-plugin')
-const common = require('./webpack.common.js')
-
-module.exports = merge(common,{
-    plugins:[
-        new UgligyJSPlugin()
-    ]
-})
--- examples/step-1/src/subPageA.js
+++
@@ -1,2 +0,0 @@
-import './moduleA'
-export default 'subPageA'
