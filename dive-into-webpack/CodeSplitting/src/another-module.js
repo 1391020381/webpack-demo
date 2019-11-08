9acf427341559a71b0f066e898049b75c7d43ff8
zhoujin <zhoujin@iboxpay.com>
2019-11-08 18:07:30 +0800

    webpack-org

--- dive-into-webpack/CodeSplitting/src/another-module.js
+++
@@ -1,2 +0,0 @@
-import _ from "loadsh";
-console.log(_.join(["Another", "module", "loaded!"], " "));
--- examples/step-1/src/pageA.js
+++
@@ -1,9 +0,0 @@
-
-export default 'pageA'
-import './subPageB'
-import './subPageA'
-import * as _ from 'lodash'
-
-
-
-
